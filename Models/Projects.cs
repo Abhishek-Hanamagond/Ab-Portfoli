e2c18e483ce8e7b82f47b5ca390b03d0dc8e6249
Abhishek Hanamagond <thenameisabhi1289@gmail.com>
2025-06-30 01:46:40 +0530

    nullable check

--- Models/Projects.cs
+++ Models/Projects.cs
@@ -3,8 +3,8 @@
     public class Projects
     {
         public int ID { get; set; }  // Primary Key
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string ImagePath { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public string? ImagePath { get; set; }
     }
 }
