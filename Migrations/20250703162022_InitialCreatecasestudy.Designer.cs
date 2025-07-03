0e3fedb42171685e744c141789b6cb63392c7c0e
Abhishek Hanamagond <thenameisabhi1289@gmail.com>
2025-07-03 21:52:35 +0530

    updated db migrate

--- Migrations/20250703162022_InitialCreatecasestudy.Designer.cs
+++ Migrations/20250703162022_InitialCreatecasestudy.Designer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using portfolio.Data;
@@ -11,9 +12,11 @@ using portfolio.Data;
 namespace portfolio.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250703162022_InitialCreatecasestudy")]
+    partial class InitialCreatecasestudy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
